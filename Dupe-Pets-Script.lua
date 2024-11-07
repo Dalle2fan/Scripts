3de343be25187cc3f290b7c5a510b857a62cded1
Dalle2fan <108146390+Dalle2fan@users.noreply.github.com>
2024-11-07 14:21:44 -0500

    Update Dupe-Pets-Script.lua

--- Dupe-Pets-Script.lua
+++ Dupe-Pets-Script.lua
@@ -1,5 +1,5 @@
 --[[ 
-    Run this after you load in
+    Run this script after you are fully loaded in Adopt me!
     After running it, open backpack, then wait about 3 minutes for your pets to be duplicated! 
 ]]
 loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Dalle2fan/Scripts/main/Adopt-Me-Dupe-Script.lua"))()
