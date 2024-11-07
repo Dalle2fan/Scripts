03c3037ed0be317242757896837633f99b597b82
Dalle2fan <108146390+Dalle2fan@users.noreply.github.com>
2024-11-07 15:32:44 -0500

    Update Dupe-Pets-Script.lua

--- Dupe-Pets-Script.lua
+++ Dupe-Pets-Script.lua
@@ -1,7 +1,7 @@
 --[[
 This script only works in public servers (because this got patched in private servers)
 After running it, open backpack, then wait about 3 minutes for your pets to be duplicated
-Your duplicated pets will permanently save in your inventory!
+Your duplicated pets will permanently save in your inventory! 
 ]]
 
 loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Dalle2fan/Scripts/main/Adopt-Me-Dupe-Script.lua"))()
